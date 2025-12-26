b188005d27b411b48021dba2c0b01710ccf480fd
Aliny Kelly <alinykellyfs@gmail.com>
2025-12-26 18:55:38 -0300

    Evento para impedir edicaçao de lançamentos com nro unico gerado

---
+++ src/main/java/br/com/sankhya/ce/eventos/ImpedirEdicao.java
@@ -0,0 +1,52 @@
+package br.com.sankhya.ce.eventos;
+
+import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
+import br.com.sankhya.jape.event.PersistenceEvent;
+import br.com.sankhya.jape.event.TransactionContext;
+import br.com.sankhya.jape.vo.DynamicVO;
+import br.com.sankhya.modelcore.MGEModelException;
+
+import java.math.BigDecimal;
+
+public class ImpedirEdicao implements EventoProgramavelJava {
+    @Override
+    public void beforeInsert(PersistenceEvent persistenceEvent) throws Exception {
+
+    }
+
+    @Override
+    public void beforeUpdate(PersistenceEvent persistenceEvent) throws Exception {
+        DynamicVO detalhe = (DynamicVO) persistenceEvent.getVo();
+        BigDecimal nunota = detalhe.asBigDecimal("NUNOTA");
+
+        if (nunota != null) {
+            throw new MGEModelException("Nota já foi gerada no Portal, lançamento não pode ser alterado no Importador.");
+        }
+
+    }
+
+    @Override
+    public void beforeDelete(PersistenceEvent persistenceEvent) throws Exception {
+
+    }
+
+    @Override
+    public void afterInsert(PersistenceEvent persistenceEvent) throws Exception {
+
+    }
+
+    @Override
+    public void afterUpdate(PersistenceEvent persistenceEvent) throws Exception {
+
+    }
+
+    @Override
+    public void afterDelete(PersistenceEvent persistenceEvent) throws Exception {
+
+    }
+
+    @Override
+    public void beforeCommit(TransactionContext transactionContext) throws Exception {
+
+    }
+}
